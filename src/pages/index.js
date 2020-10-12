9f5682c6e68f007554e7c9b0f52b887c2c972b58
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-10-12 22:45:50 +0200

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import React from "react"
+import Layout from '../components/layout'
 
-export default function Home() {
-  return <div>Hello world!</div>
-}
+export default () => (
+  <div>
+    <Layout />
+  </div>
+)
