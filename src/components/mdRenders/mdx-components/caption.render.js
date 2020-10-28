d9669f22a0df5ab45723775217495e7813a16dd1
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-10-28 19:08:58 +0100

    WIP

--- src/components/mdRenders/mdx-components/caption.render.js
+++ src/components/mdRenders/mdx-components/caption.render.js
@@ -5,9 +5,10 @@ const CaptionRender = ({ link, label }) => {
   if (link) {
     const baseGitUrl =
       "https://raw.githubusercontent.com/cangulo/cangulo-site-mdx/master/"
+
     const urlToFile = link.includes("http") ? link : baseGitUrl.concat(link)
 
-    https: return (
+    return (
       <Link to={urlToFile}>
         <p style={{ textAlign: "center" }}>{label ?? `Link to the file`}</p>
       </Link>
