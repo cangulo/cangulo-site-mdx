f3959e126da497c4e2fdbb0d82dd7a2ca105ee89
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-10-28 23:52:58 +0100

    WIP

--- src/components/mdRenders/mdx-provider.js
+++ src/components/mdRenders/mdx-provider.js
@@ -2,7 +2,6 @@ import React from "react"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import CodeBlock from "./mdx-renders/code-block"
-import ImgRender from "./mdx-components/img-render"
 import AlertRender from "./mdx-components/alert-render"
 import CaptionRender from "./mdx-components/caption.render"
 
@@ -10,7 +9,6 @@ const shortcodes = {
   pre: props => <div className="gatsby-highlight" {...props} />,
   code: CodeBlock,
   AlertRender,
-  ImgRender,
   CaptionRender,
 }
 
