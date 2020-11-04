ea2b464b9f69e3d05c841fc35eadc7de62314e5f
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-11-04 23:07:51 +0100

    WIP

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,5 +41,58 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.description,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(filter: {fields: {postType: {in: ["cheatsheets", "blog", "meetups"]}}}, sort: {fields: [frontmatter___date], order: DESC}) {
+                  edges {
+                    node {
+                      slug
+                      frontmatter {
+                        title
+                        description
+                        date(formatString: "DD/MM/YYYY")
+                      }
+                      html
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Carlos Angulo Blog RSS",
+            // optional configuration to insert feed reference in pages:
+            // match: "^/(blog)|(cheatsheets)|(meetups)/",
+          },
+        ],
+      },
+    },
   ],
 }
