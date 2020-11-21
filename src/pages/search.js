031584eefed9eca6dd58f77c751c35ad17f1ceb6
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-11-22 00:06:07 +0100

    Added Search Page

---
+++ src/pages/search.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout/layout"
+import SearchArea from "../components/search/search"
+
+export default function Search() {
+  return (
+    <Layout title="Search" imgRelativePath="./page-search.jpg">
+      <SearchArea />
+    </Layout>
+  )
+}
