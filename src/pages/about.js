2e59e1e35cdff701495b368cea720476c02bae5f
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-11-23 02:29:28 +0100

    WIP

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,6 +6,20 @@ const ContactPage = ({ location }) => (
   <div>
     <Layout title="About" location={location}>
       <AboutMeArea />
+      {/* <div>
+        HOLA
+        <div
+          data-iframe-width="150"
+          data-iframe-height="270"
+          data-share-badge-id="80e4c7dd-56c7-451a-8e76-d138e1158e75"
+          data-share-badge-host="https://www.youracclaim.com"
+        ></div>
+        <script
+          type="text/javascript"
+          async
+          src="https://cdn.youracclaim.com/assets/utilities/embed.js"
+        ></script>
+      </div> */}
     </Layout>
   </div>
 )
