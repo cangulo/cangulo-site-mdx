b963d58362586b9b6cff6b1aa6ccc15f12d110e5
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2020-11-25 01:36:43 +0100

    WIP

--- src/components/aboutme/aboutme-certifications-badges.js
+++ src/components/aboutme/aboutme-certifications-badges.js
@@ -1,12 +1,10 @@
 import React from "react"
 import { Col, Container, Row } from "react-bootstrap"
 import AwsCertifiedDeveloperAssociate from "./certifications/aws-cert-developer"
-import HelmetIframeScripts from "./certifications/helmet-iframe-certifications"
 import ScrumtMasterPUE2019 from "./certifications/scrum-master-2019"
 
 const AboutMeCertificationsBadges = () => (
   <div>
-    <HelmetIframeScripts />
     <h3>Badges</h3>
     <Container fluid>
       <Row
