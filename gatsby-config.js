85ee8333830705a729b721b5aad400d351e06626
Carlos Angulo Mascarell <carlos.angulo.mascarell@outlook.com>
2020-12-22 13:24:52 +0100

    Added RSS Feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,29 @@
 var _ = require("lodash")
 
+function JoinPostParameters(
+  slug,
+  title,
+  description,
+  tags,
+  postSerie,
+  siteUrl
+) {
+  let result = `<div><h1>${title}</h1>`
+  result += `<p>${description}</p><span> Check the article <a href="${siteUrl}/${slug}">here</a></span>`
+  if (tags) {
+    let links = tags.map(tag => `<a href="${siteUrl}/tags/${tag}">${tag}</a>`)
+    links.push(`<a href="${siteUrl}/tags">All tags</a>`)
+    result += `<p><b>Tags: </b>${links.join(", ")}</p>`
+  }
+  if (postSerie) {
+    const collectionLink = `<a href="${siteUrl}/collections/${postSerie}">${postSerie}</a>`
+    const allCollectionsLink = `<a href="${siteUrl}/collections">All collections</a>`
+    result += `<p><b>Collection: </b>${collectionLink}, ${allCollectionsLink}</p>`
+  }
+  result += `</div>`
+  return result
+}
+
 module.exports = {
   siteMetadata: {
     title: "Welcome!",
@@ -61,6 +85,67 @@ module.exports = {
           },
         },
         filter: (node, getNode) => node.frontmatter.tags,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-feed-mdx`,
+      options: {
+        query: `
+        {
+          site {
+            siteMetadata {
+              title
+              description
+              siteUrl
+              site_url: siteUrl
+            }
+          }
+        }
+      `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMdx } }) => {
+              return allMdx.edges.map(edge => {
+                const postContent = JoinPostParameters(
+                  edge.node.slug,
+                  edge.node.frontmatter.title,
+                  edge.node.frontmatter.description,
+                  edge.node.frontmatter.tags,
+                  edge.node.fields.postSerie,
+                  site.siteMetadata.siteUrl
+                )
+                return Object.assign({}, edge.node.frontmatter, {
+                  url: site.siteMetadata.siteUrl + "/" + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + "/" + edge.node.slug,
+                  custom_elements: [{ "content:encoded": postContent }],
+                })
+              })
+            },
+            query: `
+              {
+                allMdx(sort: {order: DESC, fields: [frontmatter___date]}, 
+                  filter: {fields: {postType: {in: ["blog","meetups"]}}}) {
+                  edges {
+                    node {
+                      slug
+                      frontmatter {
+                        title
+                        description
+                        date
+                        tags
+                      }
+                      fields {
+                        postSerie
+                      }
+                    }
+                  }
+                }
+              }
+          `,
+            output: "/rss.xml",
+            title: "Carlos Angulo Blog",
+          },
+        ],
       },
     },
   ],
